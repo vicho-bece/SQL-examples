00db9c25d75839e7e406241261828472a9203392
vicho-bece <vicentebecerra72@gmail.com>
2026-08-13 00:17:25 -0400

    Update Funciones y Triggers.sql
    Tabla que almacena las consultas ejecutadas, registrando el usuario, fecha y hora.
    
    Funcion y Triggers para registrar toda consulta (INSERT, UPDATE o DELETE) que se aplique a la tabla tiendas.

--- Bike_store/Funciones y Triggers.sql
+++ Bike_store/Funciones y Triggers.sql
@@ -1,15 +1,16 @@
--- TRIGGER PARA IMPEDIR QUE UN USUARIO ELIMINE DATOS DE LA TABLA PRODUCTOS
+-- FUNCION PARA IMPEDIR QUE UN USUARIO ELIMINE DATOS DE LA TABLA PRODUCTOS
 CREATE OR REPLACE FUNCTION fn_prevenir_delete()
 RETURNS TRIGGER
 AS
 $$
 BEGIN
+	-- Mensaje cuando se detecte un DELETE en la tabla
 	RAISE EXCEPTION 'Eliminar de la tabla PRODUCTOS no esta permitido';
 END;
 $$
 LANGUAGE plpgsql;
 
-
+-- TRIGGER QUE EJECUTA LA FUNCION "fn_prevenir_delete" AL DETECTAR UNA CONSULTA DELETE EN LA TABLA  PRODUCTOS
 CREATE TRIGGER no_eliminar_productos
 BEFORE DELETE ON productos
 FOR EACH ROW
@@ -17,6 +18,8 @@ EXECUTE FUNCTION fn_prevenir_delete();
 
 DELETE FROM productos;
 
+-- FUNCION PARA EVITAR QUE UN USUARIO ELIMINE TODOS LOS REGISTROS DE LA TABLA CLIENTE
+-- La finalidad es prevenir que realicen " DELETE FROM clientes "
 CREATE OR REPLACE FUNCTION fn_no_eliminar_todos()
 RETURNS TRIGGER
 AS
@@ -27,7 +30,9 @@ DECLARE
 BEGIN
 	SELECT COUNT(*) INTO total_rows FROM clientes;
 
+	-- Si la consulta del DELETE deja vacio la tabla clientes, activa el RAISE EXCEPTION
 	IF total_rows = 0 THEN
+		-- Anula la transaccion
 		RAISE EXCEPTION 'ADVERTENCIA: Ejecutaste un DELETE que elimina todas las filas de la tabla CLIENTES';
 	END IF;
 	RETURN NULL;
@@ -35,9 +40,46 @@ END;
 $$
 LANGUAGE plpgsql;
 
+-- TRIGGER QUE EJECUTA LA FUNCION " fn_no_eliminar_todos " DESPUES DE UNA CONSULTA DELETE EN LA TABLA CLIENTES.
 CREATE TRIGGER no_eliminar_todos
 AFTER DELETE ON clientes FOR EACH STATEMENT
 EXECUTE FUNCTION fn_no_eliminar_todos();
 
 DELETE FROM clientes;
 SELECT * FROM clientes;
+
+-- CREACION DE TABLA QUE REGISTRA LA ACTIVIDAD DE LOS USUARIOS (No se relaciona con la temática de las bicicletas)
+CREATE TABLE actividad(
+	usuario VARCHAR(250) not null,
+	fecha DATE not null,
+	hora TIME not null,
+	consulta TEXT not null
+);
+
+-- FUNCION PARA ALMACENAR FECHA Y HORA DE CUALQUIER CONSULTA APLICADA POR UN USUARIO
+CREATE OR REPLACE FUNCTION fn_detectar_actividad()
+RETURNS TRIGGER
+AS
+$$
+DECLARE
+	nombre_usuario VARCHAR(250) := USER;
+	fecha_actual DATE := current_date;
+	hora_actual TIME := current_time;
+	consulta TEXT := current_query();
+
+BEGIN
+	INSERT INTO actividad VALUES(nombre_usuario, fecha_actual, hora_actual, consulta);
+	RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+-- TRIGGER: ACTIVA LA FUNCION DESPUES DE UNA CONSULTA A LA TABLA TIENDAS.
+drop trigger IF EXISTS user_activity ON tiendas;
+CREATE TRIGGER user_activity
+AFTER INSERT OR UPDATE OR DELETE ON tiendas
+FOR EACH ROW
+EXECUTE PROCEDURE fn_detectar_actividad();
+
+INSERT INTO tiendas VALUES(4, 'Tienda Bicicleta', '123456897', NULL, 'Armando Casas 001', 'Nueva York', 'NY', NULL);
+SELECT * FROM actividad
