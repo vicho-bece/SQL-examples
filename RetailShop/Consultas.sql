52e360edd77f0a1019d676832b5ea079d0c556ab
vicho-bece <vicentebecerra72@gmail.com>
2026-08-18 19:21:36 -0400

    Update Consultas.sql

--- RetailShop/Consultas.sql
+++ RetailShop/Consultas.sql
@@ -1,7 +1,7 @@
 -- CONSULTA SELECCIONAR TODAS LAS COLUMNAS
 SELECT * FROM CLIENTES;
 SELECT * FROM PRODUCTOS;
-SELECT * FROM ORDENES;
+SELECT * FROM ORDENES ORDER BY fecha_orden;
 
 -- OBTENER EL NÚMERO DE CLIENTES POR CIUDAD
 SELECT
@@ -60,3 +60,60 @@ PIVOT
 	COUNT(id_cliente)
 	FOR ciudad_cliente IN ([Liverpool], [London], [Bristol], [Nottingham], [Sheffield], [Birmingham], [Manchester], [Leeds])
 ) AS pvt;
+
+
+-- OBTENER LOS VALORES UNICOS DE CATEGORIAS DE PRODUCTOS
+SELECT DISTINCT categoria_producto
+FROM PRODUCTOS;
+
+
+-- OBTENER EL REGISTRO DE UNIDADES VENDIDAS POR SU CATEGORIAS EN FECHAS REGISTRADAS
+SELECT 
+	fecha_orden,
+	[Beauty] AS Beauty,
+	[Clothing] AS Clothing,
+	[Electronics] AS Electronics,
+	[Home] AS Home,
+	[Sports] AS Sports
+FROM
+( 
+	SELECT 
+		O.fecha_orden as Fecha_orden, 
+		P.categoria_producto as Categorias,
+		O.cantidad_prod as Cantidad
+	FROM ORDENES AS O
+	INNER JOIN PRODUCTOS AS P ON P.id_producto = O.ref_producto
+) p
+PIVOT
+(
+	SUM(Cantidad)
+	FOR Categorias IN ([Beauty], [Clothing], [Electronics], [Home], [Sports])
+) AS pvt ORDER BY Fecha_orden;
+
+
+
+-- OBTENER EL REGISTRO DE LAS GANANCIAS POR SU CATEGORIAS CADA MES
+-- APLICANDO EL FILTRO DEL AÑO
+SELECT 
+	Mes,
+	[Beauty] AS Beauty,
+	[Clothing] AS Clothing,
+	[Electronics] AS Electronics,
+	[Home] AS Home,
+	[Sports] AS Sports
+FROM
+( 
+	SELECT 
+		MONTH(O.fecha_orden) AS NumMes,
+		DATENAME(MONTH, DATEFROMPARTS(2000, MONTH(O.fecha_orden), 1)) as Mes, 
+		P.categoria_producto as Categorias,
+		(O.cantidad_prod * P.precio_producto) as Ganancias
+	FROM ORDENES AS O
+	INNER JOIN PRODUCTOS AS P ON P.id_producto = O.ref_producto
+	WHERE YEAR(O.fecha_orden) = '2024'
+) p
+PIVOT
+(
+	SUM(Ganancias)
+	FOR Categorias IN ([Beauty], [Clothing], [Electronics], [Home], [Sports])
+) AS pvt ORDER BY NumMes;
